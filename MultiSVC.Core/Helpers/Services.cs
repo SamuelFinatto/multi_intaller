ee289e81504b3d315eb4aca22ea7534ee49104a4
samuelfinatto <samueldfinatto@gmail.com>
2020-08-30 23:02:35 -0300

    First commit to Multi Installer personal project

---
+++ MultiSVC.Core/Helpers/Services.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiSVC.Core.Helpers
+{
+    public class Services
+    {
+
+        
+
+    }
+}
---
+++ MultiSVCInstaller/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MultiSVCInstaller
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            MultiSVC.Core.Helpers.ServiceInstaller installer = new MultiSVC.Core.Helpers.ServiceInstaller();
+
+            var result = installer.InstallService(@"C:\Users\samue\AppData\Roaming\uTorrent\uTorrent.exe", "TestingService", "Service DisplayName", "Super description");
+
+            var rresult = installer.UnInstallService("TestingService");
+        }
+    }
+}
