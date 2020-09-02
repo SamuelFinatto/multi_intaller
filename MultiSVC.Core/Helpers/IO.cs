1a24af5cce17b1015cf4d60cb140a1d327804acc
samuelfinatto <samueldfinatto@gmail.com>
2020-09-01 22:17:32 -0300

    Include IO methods; Include project and tests

---
+++ MultiSVC.Core/Helpers/IO.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MultiSVC.Core.Helpers
+{
+    public static class IO
+    {
+        public static bool CheckFolderAccess(string folderPath)
+        {
+            var logger = NLog.LogManager.GetCurrentClassLogger();
+
+            if (!Directory.Exists(folderPath))
+                return false;
+
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(folderPath);
+                var folderName = directoryInfo.Name;
+                var date = DateTime.Now.ToString("hhmmss");
+                directoryInfo.RenameTo($"{folderName}.{date}");
+                logger.Info("Folder is enable to access");
+                directoryInfo.RenameTo(folderName);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                logger.Error(ex, "Folder unable to be accessed.");
+                return false;
+            }
+        }
+
+        public static void RenameTo(this DirectoryInfo di, string name)
+        {
+            if (di == null)
+            {
+                throw new ArgumentNullException("di", "Directory info to rename cannot be null");
+            }
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("New name cannot be null or blank", "name");
+            }
+
+            di.MoveTo(Path.Combine(di.Parent.FullName, name));
+
+            return; //done
+        }
+    }
+}
---
+++ MultiSVC.Tests/IOTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using MultiSVC.Core.Helpers;
+
+namespace MultiSVC.Tests
+{
+    [TestClass]
+    public class IOTests
+    {
+        [TestMethod]
+        public void CheckFolderAccess_insertnull_returnFalse()
+        {
+            var result = MultiSVC.Core.Helpers.IO.CheckFolderAccess(null);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CheckFolderAccess_insertvalidFolder_returnTrue()
+        {
+            var folderinfo = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), $"folder{DateTime.Now:hhmmss}"));
+            var result = MultiSVC.Core.Helpers.IO.CheckFolderAccess(folderinfo.FullName);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CheckFolderAccess_insertvalidFolderWithFileOpen_returnFalse()
+        {
+            var folderinfo = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), $"folder{DateTime.Now:hhmmss}"));
+            bool result = false;
+            using (FileStream fs = File.Open(Path.Combine(folderinfo.FullName,"file.file"), FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes("This is some text in the file.");
+
+                result = MultiSVC.Core.Helpers.IO.CheckFolderAccess(folderinfo.FullName);
+                // Add some information to the file.
+                fs.Write(info, 0, info.Length);
+            }
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void RenameTo_InsertCorrectString_RenameOK()
+        {
+            var folderinfo = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), $"folder{DateTime.Now:hhmmss}"));
+            folderinfo.RenameTo($"folderrrr{DateTime.Now:hhmmss}");
+        }
+
+        [TestMethod]
+        public void RenameTo_InsertNull_Fail()
+        {
+            var folderinfo = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), $"folder{DateTime.Now:hhmmss}"));
+            Assert.ThrowsException<ArgumentException>(() => folderinfo.RenameTo(null));
+        }
+    }
+}
